8d6c9c680364ba85ea1733088cf322782899b204
Leejun Liao <liaoleejun@163.com>
2019-02-16 23:48:41 +0800

    添加README
    * 修改样式, 通过js修改

--- script/SimpleANOH.js
+++ script/SimpleANOH.js
@@ -19,7 +19,7 @@ function addIndex() {
         indices[hIndex]++;
 
         // display the full position in the hierarchy
-        jQuery(this).prepend("<span style='color: #ccc;'>" + indices.join(".") + "." + "</span>" + "&emsp;&emsp;&emsp;");
+        jQuery(this).prepend("<span style='color: #ccc; margin-right: 10px'>" + indices.join(".") + "." + "</span>"  + "&emsp;");
     });
 }
 
